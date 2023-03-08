c48877c308afedce7c80455e2c4f1eefef8a47c4
EvgenyaHramova <evgenya.hramova@yandex.ru>
2023-03-08 11:11:33 +0400

    new fail zadacha_2.java

---
+++ zadacha_2.java
@@ -0,0 +1,19 @@
+public class zadacha_2 {
+    public static void main(String[] args) {
+        System.out.println("Простые числа в интервале от 1 до 1000:");
+        for (int i = 2; i < 1000; i++){
+            int num = 0;
+            for (int j = 2; j < i; j++){
+                if ((i % j) != 0){
+                    num += 0;
+                } else {
+                    num +=1;
+                }
+            }
+            if (num == 0){
+                System.out.print(i + ", ");
+            }
+
+        }
+    }
+}
